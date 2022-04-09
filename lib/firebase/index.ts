91388bc31167c237a63ee990494e3ca64c96de9c
Timothy Meade <tim@do.dev>
2022-04-09 10:57:42 -0400

    redid env

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -13,18 +13,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyC0g6MWpu9296YnzhuaJDaQ7sGTvj2igNo',
-  authDomain: 'doc-dev-9f330.firebaseapp.com',
-  projectId: 'doc-dev-9f330',
-  storageBucket: 'doc-dev-9f330.appspot.com',
-  messagingSenderId: '960879673571',
-  appId: '1:960879673571:web:66cb184560d627671e45d6',
-  // apiKey: process.env.FIREBASE_APIKEY,
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
